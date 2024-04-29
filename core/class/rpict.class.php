09d9501863da887d723834d8530f64631a12ea00
Thomas Lier <thomas.lier@vitalliance.fr>
2024-04-29 17:20:15 +0200

    Update php class

--- core/class/rpict.class.php
+++ core/class/rpict.class.php
@@ -1,26 +1,25 @@
 <?php
 
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 require_once __DIR__ . '/../../../../core/php/core.inc.php';
 
 class rpict extends eqLogic
 {
-
     public static function getRpictInfo($_url)
     {
         $return = self::deamon_info();
@@ -29,53 +28,116 @@ class rpict extends eqLogic
         }
     }
 
-    public static function createFromDef(string $nid)
+
+    public static function changeLogLive($level)
     {
-		$rpict = rpict::byLogicalId($nid, 'rpict');
-		if (!is_object($rpict)) {
-			$eqLogic = (new rpict())
-					->setName($nid);
-		}
-		$eqLogic->setLogicalId($nid)
-				->setEqType_name('rpict')
-				->setIsEnable(1)
-				->setIsVisible(1);
-		$eqLogic->save();
-		return $eqLogic;
+        $activation_Modem = (config::byKey('activation_Modem', 'teleinfo') == "") ? 1 : config::byKey('activation_Modem', 'teleinfo');
+        if ($activation_Modem == '0') {
+            log::add('teleinfo', 'info', 'pas d envoi de message faute de configuration');
+            return false;
+        }
+        sleep(1); // attend que le level ait eu le temps de s'écrire dans la bdd
+        $value['cmd'] = 'changelog';
+        $value['level'] = log::convertLogLevel(log::getLogLevel('teleinfo'));
+        $socketport = config::byKey('socketport', __CLASS__, '55062');
+        $value['apikey'] = jeedom::getApiKey(__CLASS__);
+        if ($activation_Modem == 1) {
+            self::sendToDaemon($value, 'serial', $socketport);
+        }
     }
 
-    public static function createCmdFromDef($_oNId, $_oKey, $_oValue)
+    public static function sendToDaemon($params, $mode, $socketport)
+    { // le mode peut être serial, mqtt ou prod
+        $deamon_info = self::deamon_info();
+        if ($deamon_info['state'] != 'ok') {
+            throw new Exception("Le démon " . $mode . " n'est pas démarré");
+        }
+        $params['apikey'] = jeedom::getApiKey('teleinfo');
+        $payLoad = json_encode($params);
+        $socket = socket_create(AF_INET, SOCK_STREAM, 0);
+        socket_connect($socket, config::byKey('sockethost', 'teleinfo', '127.0.0.1'), $socketport);
+        socket_write($socket, $payLoad, strlen($payLoad));
+        socket_close($socket);
+        return true;
+    }
+
+    /**
+     * Test si la version est béta
+     * @param bool $text
+     * @return $isBeta
+     */
+    public static function isBeta($text = false)
     {
-        if (!isset($_oKey)) {
-            log::add('rpict', 'error', 'Information manquante pour ajouter l\'équipement : ' . print_r($_oKey, true));
+        $plugin = plugin::byId('teleinfo');
+        $update = $plugin->getUpdate();
+        $isBeta = false;
+        if (is_object($update)) {
+            $version = $update->getConfiguration('version');
+            $isBeta = ($version && $version != 'stable');
+        }
+
+        if ($text) {
+            return $isBeta ? 'beta' : 'stable';
+        }
+        return $isBeta;
+    }
+
+    /**
+     * Creation objet sur reception de trame
+     * @param string $adco
+     * @return eqLogic
+     */
+    public static function createFromDef(string $nid)
+    {
+        $rpict = rpict::byLogicalId($nid, 'rpict');
+        if (!is_object($rpict)) {
+            $eqLogic = (new rpict())
+                ->setName($nid);
+        }
+        $eqLogic->setLogicalId($nid)
+            ->setEqType_name('rpict')
+            ->setIsEnable(1)
+            ->setIsVisible(1);
+        $eqLogic->save();
+        return $eqLogic;
+    }
+
+    /**
+     * Creation commande sur reception de trame
+     * @param $oADCO identifiant compteur
+     * @param $oKey etiquette
+     * @param $oValue valeur
+     * @return Commande
+     */
+    public static function createCmdFromDef($oNId, $oKey, $oValue)
+    {
+        if (!isset($oKey) || !isset($oNId)) {
+            log::add('teleinfo', 'error', '[RPICT]-----Information manquante pour ajouter l\'équipement : ' . print_r($oKey, true) . ' ' . print_r($oNId, true));
             return false;
         }
-        if (!isset($_oNId)) {
-            log::add('rpict', 'error', 'Information manquante pour ajouter l\'équipement : ' . print_r($_oNId, true));
-            return false;
-        }
-        $rpict = rpict::byLogicalId($_oNId, 'rpict');
+        $rpict = rpict::byLogicalId($oNId, 'rpict');
         if (!is_object($rpict)) {
             return false;
         }
         if ($rpict->getConfiguration('AutoCreateFromCompteur') == '1') {
-            log::add('rpict', 'info', 'Création de la commande ' . $_oKey . ' sur le NodeID ' . $_oNId);
+            log::add('rpict', 'info', 'Création de la commande ' . $oKey . ' sur le NodeID ' . $oNId);
             $cmd = (new rpictCmd())
-                    ->setName($_oKey)
-                    ->setLogicalId($_oKey)
-                    ->setType('info');
-            $cmd->setEqLogic_id($rpict->id);
-            $cmd->setConfiguration('info_rpict', $_oKey);
+                ->setName($oKey)
+                ->setLogicalId($oKey)
+                ->setType('info');
+
             $cmd->setSubType('numeric')
-                    ->setDisplay('generic_type', 'GENERIC_INFO');
-            $cmd->setIsHistorized(0)
-                    ->setIsVisible(0);
+                ->setDisplay('generic_type', 'GENERIC_INFO');
+
+            $cmd->setEqLogic_id($rpict->id);
+            $cmd->setConfiguration('info_rpict', $oKey);
+            $cmd->setIsHistorized(0)->setIsVisible(0);
             $cmd->save();
-            $cmd->event($_oValue);
+            $cmd->event($oValue);
             return $cmd;
         }
     }
-    
+
     /**
      * 
      * @param type $debug
@@ -83,51 +145,60 @@ class rpict extends eqLogic
      */
     public static function runDeamon($debug = false)
     {
-        log::add('rpict', 'info', 'Démarrage RPICT');
-        $rpictPath            = realpath(dirname(__FILE__) . '/../../ressources');
-        $modemSerieAddr       = config::byKey('port', 'rpict');
+        $rpictPath = realpath(dirname(__FILE__) . '/../../ressources');
+        $activation_Modem = (config::byKey('activation_Modem', 'teleinfo') == "") ? 1 : config::byKey('activation_Modem', 'teleinfo');
+        if ($activation_Modem == '') {
+            $activation_Modem = 1;
+            log::add('rpict', 'info', '---------- Activation Modem 1---------');
+        }
+        log::add('rpict', 'info', 'Démarrage daemon ');
+
+        log::add('rpict', 'info', 'Démarrage compteur ');
         $modemVitesse         = config::byKey('modem_vitesse', 'rpict');
-        if ($modemSerieAddr == "serie") {
+        $socketPort           = config::byKey('socketport', 'rpict', '55062');
+        if (config::byKey('port', 'rpict') == "serie") {
             $port = config::byKey('modem_serie_addr', 'rpict');
         } else {
             $port = jeedom::getUsbMapping(config::byKey('port', 'rpict'));
-			if (!file_exists($port)) {
-				log::add('rpict', 'error', 'Le port n\'existe pas');
-				return false;
-			}
-			$cle_api = config::byKey('api');
-			if ($cle_api == '') {
-				log::add('rpict', 'error', 'Erreur de clé api, veuillez la vérifier.');
-				return false;
-			}
+            if (!file_exists($port)) {
+                log::add('rpict', 'error', 'Le port n\'existe pas');
+                return false;
+            }
         }
-		if ($modemVitesse == "") {
-			$modemVitesse = '38400';
-		}
-        
-        exec('sudo chmod 777 ' . $port . ' > /dev/null 2>&1'); // TODO : Vérifier dans futur release si tjs nécessaire
+        if ($modemVitesse == "") {
+            $modemVitesse = '38400';
+        }
 
-        log::add('rpict', 'info', '--------- Informations sur le master --------');
-        log::add('rpict', 'info', 'Debug : ' . $debug);
+        exec('sudo chmod 777 ' . $port . ' > /dev/null 2>&1');
+
+        log::add('teleinfo', 'info', '---------- Informations de lancement ---------');
         log::add('rpict', 'info', 'Port modem : ' . $port);
-        log::add('rpict', 'info', 'Vitesse modem : ' . $modemVitesse);
-        $debug     = ($debug) ? "1" : "0";
+        log::add('rpict', 'info', 'Socket : ' . $socketPort);
         log::add('rpict', 'info', '---------------------------------------------');
 
-		$rpictPath = $rpictPath . '/rpict.py';
-		$cmd          = 'nice -n 19 /usr/bin/python ' . $rpictPath . ' -d ' . $debug . ' -p ' . $port . ' -v ' . $modemVitesse . ' -c ' . config::byKey('api') . ' -r ' . realpath(dirname(__FILE__));
-        
+        $cmd          = 'nice -n 19 ' . $rpictPath . '/venv/bin/python3 ' . $rpictPath . '/rpict.py';
+        //$cmd          = 'nice -n 19 /usr/bin/python3 ' . $rpictPath . '/rpict.py';
+        $cmd         .= ' --port ' . $port;
+        $cmd         .= ' --vitesse ' . $modemVitesse;
+        $cmd         .= ' --apikey ' . jeedom::getApiKey('rpict');
+        $cmd         .= ' --socketport ' . $socketPort;
+        $cmd         .= ' --cycle ' . config::byKey('cycle', 'rpict', '0.3');
+        $cmd         .= ' --callback ' . network::getNetworkAccess('internal', 'proto:127.0.0.1:port:comp') . '/plugins/rpict/core/php/jeeRpict.php';
+        $cmd         .= ' --cyclesommeil ' . config::byKey('cycle_sommeil', 'rpict', '0.5');
+        $cmd         .= ' --loglevel ' . log::convertLogLevel(log::getLogLevel(__CLASS__));
+
+
         log::add('rpict', 'info', 'Exécution du service : ' . $cmd);
-        $result = exec('nohup ' . $cmd . ' >> ' . log::getPathToLog('rpict') . ' 2>&1 &');
+        $result = exec('nohup ' . $cmd . ' >> ' . log::getPathToLog('rpict_deamon') . ' 2>&1 &');
         if (strpos(strtolower($result), 'error') !== false || strpos(strtolower($result), 'traceback') !== false) {
-            log::add('rpict', 'error', $result);
+            log::add('rpict', 'error', '[RPICT]-----' . $result);
             return false;
         }
         sleep(2);
         if (!self::deamonRunning()) {
             sleep(10);
             if (!self::deamonRunning()) {
-                log::add('rpict', 'error', 'Impossible de lancer le démon RPICT, vérifiez le port', 'unableStartDeamon');
+                log::add('rpict', 'error', '[RPICT] Impossible de lancer le démon RPICT, vérifiez le port', 'unableStartDeamon');
                 return false;
             }
         }
@@ -135,7 +206,7 @@ class rpict extends eqLogic
         log::add('rpict', 'info', 'Service OK');
         log::add('rpict', 'info', '---------------------------------------------');
     }
-    
+
 
     /**
      * 
@@ -143,14 +214,14 @@ class rpict extends eqLogic
      */
     public static function deamonRunning()
     {
-		$result = exec("ps aux | grep rpict.py | grep -v grep | awk '{print $2}'");
-		if ($result != "") {
-			return true;
-		}
-		log::add('rpict', 'info', 'Vérification de l\'état du service : NOK ');
-		return false;
+        $result = exec("ps aux | grep rpict.py | grep -v grep | awk '{print $2}'");
+        if ($result != "") {
+            return true;
+        }
+        log::add('rpict', 'info', '[deamonRunning] Vérification de l\'état du service : NOK ');
+        return false;
     }
-    
+
     /**
      * 
      * @return array
@@ -160,16 +231,28 @@ class rpict extends eqLogic
         $return               = array();
         $return['log']        = 'rpict';
         $return['state']      = 'nok';
-		$pidFile = '/tmp/rpict.pid';
+        $pidFile = jeedom::getTmpFolder('rpict') . '/rpict.pid';
         if (file_exists($pidFile)) {
             if (posix_getsid(trim(file_get_contents($pidFile)))) {
-                $return['state'] = 'ok';
+                log::add('rpict', 'debug', '[RPICT_deamon_infoserial] démon port modem 1 => ok');
+                $returnmodem = 'ok';
             } else {
+                log::add('rpict', 'error', "[RPICT_deamon_infoserial] le deamon port modem 1 s'est éteint");
+                $returnmodem = 'nok';
                 shell_exec('sudo rm -rf ' . $pidFile . ' 2>&1 > /dev/null;rm -rf ' . $pidFile . ' 2>&1 > /dev/null;');
             }
         }
 
         $return['launchable'] = 'ok';
+        if ($returnmodem != 'nok') {
+            $return['state'] = 'ok';
+            $return['deamon_modem'] = $returnmodem;
+        } else {
+            $return['state'] = 'nok';
+            $return['deamon_modem'] = $returnmodem;
+        }
+        log::add('teleinfo', 'debug', '[TELEINFO_deamon_modem] état : ' . $returnmodem);
+        log::add('teleinfo', 'debug', '[TELEINFO_deamon] état global => retour: ' . $return['state']);
         return $return;
     }
 
@@ -178,8 +261,10 @@ class rpict extends eqLogic
      */
     public static function deamon_start($debug = false)
     {
+        log::add('rpict', 'info', '[deamon_start_modem] Démarrage du service');
         if (config::byKey('port', 'rpict') != "") {    // Si un port est sélectionné
             if (!self::deamonRunning()) {
+                log::add('rpict', 'info', 'Lancement carte');
                 self::runDeamon($debug);
             }
             message::removeAll('rpict', 'noRpictPort');
@@ -193,37 +278,38 @@ class rpict extends eqLogic
      */
     public static function deamon_stop()
     {
-        log::add('rpict', 'info', '[deamon_stop] Arret du service');
+        $deamonKill = false;
         $deamonInfo = self::deamon_info();
-        if ($deamonInfo['state'] == 'ok') {
-			$pidFile = '/tmp/rpict.pid';
-			if (file_exists($pidFile)) {
-				$pid  = intval(trim(file_get_contents($pidFile)));
-				$kill = posix_kill($pid, 15);
-				usleep(500);
-				if ($kill) {
-					return true;
-				} else {
-					system::kill($pid);
-				}
-			}
-			system::kill('rpict.py');
-			$port = config::byKey('port', 'rpict');
-			if ($port != "serie") {
-				$port = jeedom::getUsbMapping(config::byKey('port', 'rpict'));
-				system::fuserk(jeedom::getUsbMapping($port));
-				sleep(1);
-			}
+        if ($deamonInfo['deamon_modem'] == 'ok') {
+            $pidFile = jeedom::getTmpFolder('rpict') . '/rpict.pid';
+            if (file_exists($pidFile)) {
+                $pid  = intval(trim(file_get_contents($pidFile)));
+                $kill = posix_kill($pid, 15);
+                usleep(500);
+                if ($kill) {
+                    $deamonKill = true;
+                    log::add('rpict', 'info', "[deamon_stop_serial] arrêt du service OK");
+                } else {
+                    system::kill($pid);
+                }
+            }
+        }
+        system::kill('rpict.py');
+        $port = config::byKey('port', 'rpict');
+        if ($port != "serie") {
+            $port = jeedom::getUsbMapping(config::byKey('port', 'rpict'));
+            system::fuserk(jeedom::getUsbMapping($port));
+            sleep(1);
         }
     }
 
     public function preSave()
     {
+        log::add('rpict', 'debug', '-------- PRESAVE --------');
         $this->setCategory('energy', 1);
         $cmd = $this->getCmd('info', 'HEALTH');
         if (is_object($cmd)) {
             $cmd->remove();
-            $cmd->save();
         }
     }
 
@@ -232,7 +318,7 @@ class rpict extends eqLogic
         log::add('rpict', 'debug', '-------- Sauvegarde de l\'objet --------');
         foreach ($this->getCmd(null, null, true) as $cmd) {
             switch ($cmd->getConfiguration('info_rpict')) {
-                default :
+                default:
                     log::add('rpict', 'debug', '=> default');
                     if ($cmd->getDisplay('generic_type') == '') {
                         $cmd->setDisplay('generic_type', 'GENERIC_INFO');
@@ -240,7 +326,6 @@ class rpict extends eqLogic
                     break;
             }
         }
-        after_template:
         log::add('rpict', 'info', '==> Gestion des id des commandes');
         foreach ($this->getCmd('info') as $cmd) {
             log::add('rpict', 'debug', 'Commande : ' . $cmd->getConfiguration('info_rpict'));
@@ -250,7 +335,7 @@ class rpict extends eqLogic
         log::add('rpict', 'debug', '-------- Fin de la sauvegarde --------');
 
         if ($this->getConfiguration('AutoGenerateFields') == '1') {
-            $this->CreateFromAbo();
+            $this->AutoCreate();
         }
 
         $this->createOtherCmd();
@@ -263,28 +348,30 @@ class rpict extends eqLogic
 
     public function createOtherCmd()
     {
+        log::add('rpict', 'debug', '-------- Santé --------');
         $array = array("HEALTH");
-        for ($ii = 0; $ii < 1; $ii++) {
-            $cmd = $this->getCmd('info', $array[$ii]);
-            if ($cmd === false) {
+        foreach ($array as $value) {
+            $cmd = $this->getCmd('info', $value);
+            if (!is_object($cmd)) {
+                log::add('rpict', 'debug', 'Santé => ' . $value);
                 $cmd = new rpictCmd();
-                $cmd->setName($array[$ii]);
+                $cmd->setName($value);
                 $cmd->setEqLogic_id($this->id);
-                $cmd->setLogicalId($array[$ii]);
+                $cmd->setLogicalId($value);
                 $cmd->setType('info');
-                $cmd->setConfiguration('info_rpict', $array[$ii]);
+                $cmd->setConfiguration('info_rpict', $value);
                 $cmd->setConfiguration('type', 'health');
                 $cmd->setSubType('numeric');
                 $cmd->setUnite('Wh');
                 $cmd->setIsHistorized(0);
-                $cmd->setEventOnly(1);
+                //$cmd->setEventOnly(1);
                 $cmd->setIsVisible(0);
                 $cmd->save();
             }
         }
     }
 
-    public function CreateFromAbo($_abo)
+    public function AutoCreate()
     {
         $this->setConfiguration('AutoGenerateFields', '0');
         $this->save();
@@ -296,7 +383,7 @@ class rpict extends eqLogic
     {
         $return                  = array();
         $return['log']           = 'rpict_update';
-        $return['progress_file'] = '/tmp/rpict_in_progress';
+        $return['progress_file'] = '/tmp/jeedom/rpict/dependance';
         $return['state']         = (self::installationOk()) ? 'ok' : 'nok';
         return $return;
     }
@@ -321,7 +408,6 @@ class rpict extends eqLogic
         log::remove(__CLASS__ . '_update');
         return array('script' => __DIR__ . '/../../ressources/install_#stype#.sh ' . jeedom::getTmpFolder('rpict') . '/dependance', 'log' => log::getPathToLog(__CLASS__ . '_update'));
     }
-
 }
 
 class rpictCmd extends cmd
@@ -329,7 +415,5 @@ class rpictCmd extends cmd
 
     public function execute($_options = null)
     {
-        
     }
-
 }
